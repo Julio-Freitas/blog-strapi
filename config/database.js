3ddf207c45b7cb38fbc321a69c41dd7c303b021f
julio-freitas <julio.clf31@gmail.com>
2021-01-01 16:17:39 -0300

    fix: file database

--- config/database.js
+++ config/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => ({
       settings: {
         client: "postgres",
         host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.env("DATABASE_PORT", 5432),
+        port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "blog-strapi"),
         username: env("DATABASE_USERNAME", "postgres"),
         password: env("DATABASE_PASSWORD", "senha"),
-        ssl: env.env("DATABASE_SSL", false),
+        ssl: env.bool("DATABASE_SSL", false),
       },
       options: {},
     },
