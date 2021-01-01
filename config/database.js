6a40959794007774ab14833bdeb5c39560fbee9a
julio-freitas <julio.clf31@gmail.com>
2021-01-01 18:20:50 -0300

    try new deploy

--- config/database.js
+++ config/database.js
@@ -16,3 +16,16 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+//nzwryjjqgldnkf
+//7ae2cb8c5d09596b17383b4d525101fa6b7d28a0b3e23ad483df500dc24fb3ad
+//ec2-52-2-127-5.compute-1.amazonaws.com
+//5432
+//d7llsq7cuoi9vi
+
+/**
+ * user: qxrzciuhebbkdi
+ * pass: ab66b290f9b43f1ffae01d483901ffbf79eccbcf07728c03e4aad1d3ca633c3f
+ * host:ec2-52-200-16-99.compute-1.amazonaws.com
+ * dbname: d37ok4votujnma
+ */
