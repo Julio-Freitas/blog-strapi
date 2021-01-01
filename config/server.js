6960739e6a391f367a30f0cb824a6f7539810997
julio-freitas <julio.clf31@gmail.com>
2021-01-01 16:31:42 -0300

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("HEROKU_URL"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'cb717969ea1b519e6635b8905a6c67c2'),
+      secret: env("ADMIN_JWT_SECRET", "cb717969ea1b519e6635b8905a6c67c2"),
     },
   },
 });
